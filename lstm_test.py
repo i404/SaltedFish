b2da839542ee421dee5ed49f78e36f19888433f5
wangchenghaox <wangchenghaox@gmail.com>
2018-03-09 00:06:12 +0800

    change some para and do some experience

--- lstm_test.py
+++ lstm_test.py
@@ -17,7 +17,7 @@ def create_model():
     model.add(Dense(input_dim=100, output_dim=1))
     # model.add(Activation("linear"))
     model.add(Activation("relu"))
-    rms = optimizers.RMSprop(lr=0.0001, rho=0.9, epsilon=1e-06)
+    rms = optimizers.RMSprop(lr=0.0002, rho=0.9, epsilon=1e-06)
     # model.compile(loss="mean_squared_logarithmic_error", optimizer=rms,
     # model.compile(loss="kullback_leibler_divergence", optimizer=rms,
     model.compile(loss="mean_squared_logarithmic_error", optimizer=rms,
@@ -49,7 +49,7 @@ def run(batch_size=50):
     print('Train...')
     model.fit(x_train, y_train,
               batch_size=batch_size,
-              epochs=150,
+              epochs=200,
               validation_data=(x_test, y_test))
     m = model.evaluate(x_test, y_test, batch_size=batch_size)
     return m
@@ -66,8 +66,13 @@ if __name__ == "__main__":
     
     use sigmoid as output activation
     and use  mean_squared_logarithmic_error as loss to deal with exp in sigmoid
-    mse ~ N(3.10, 1.30) mae ~ N(0.830, 0.018)
-    maybe not converged
+        mse ~ N(3.10, 1.30) mae ~ N(0.830, 0.018)
+    maybe not converge modify lr to 0.0005:
+        mse ~ N(1.50, 1.01) mae ~ N(0.57, 0.0030)
+    lr = 0.0002, epochs = 200
+        mse ~ N(1.11, 0.11)  mae ~ N(0.44, 1.2e-04)
+    remove 1st relu:
+        mse ~ N(1.08, 0.22) mae ~ N(0.49, 7.5e-3)
     
     """
     N = 5
